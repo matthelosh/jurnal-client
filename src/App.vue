07023d7bd46d9acc2b8f3ef63f8ccc6439a808c4
matthelosh <matthelosh@gmail.com>
2020-02-01 13:17:15 +0700

    Before Install Cordova

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="img/logo-smk.png"
           transition="scale-transition"
           width="40"
         />
@@ -28,33 +28,86 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        href="javascript:void(0);"
+        v-show="!isAuth"
         text
+        @click.stop="dlgSettings = true"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Settings</span>
+        <v-icon>mdi-settings</v-icon>
+      </v-btn>
+
+      <v-btn
+        href="javascript:void(0);"
+        v-show="isAuth"
+        text
+        @click.stop="logout()"
+      >
+        <span class="mr-2">Logout</span>
+        <v-icon>mdi-logout</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
     </v-content>
+    <Settings v-model="dlgSettings"></Settings>
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+// import HelloWorld from './components/HelloWorld';
+  import Settings from './components/Settings'
+  // import axios from 'axios'
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Settings,
   },
 
   data: () => ({
     //
+    // overlay: true,
+    dlgSettings: false
   }),
+  created() {
+    this.getSekolah()
+  },
+  methods: {
+    getSekolah() {
+      // axios.get(this.$store.getters.server+'/api/sekolah')
+      //       .then(function(res){
+      //         var sekolah = res.data.data
+      //         this.$store.dispatch('SET_SEKOLAH', sekolah)
+      //       })
+      this.$store.dispatch('SET_SEKOLAH', 'tes')
+    },
+    logout(){
+      this.$store.dispatch('AUTH_LOGOUT')
+      this.$router.push('/login')
+    }
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.isLoggedIn
+    },
+    overlay(){
+      return this.$store.getters.overlay
+    }
+  },
+  // watch: {
+  //     overlay (val) {
+  //       val && setTimeout(() => {
+  //         this.overlay = false
+  //       }, 3000)
+  //     },
+  //   },
 };
 </script>
